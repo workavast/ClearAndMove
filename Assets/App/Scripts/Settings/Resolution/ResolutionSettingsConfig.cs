2d1f96ca70138fb1d2a145084cc53e090886e236
Rodion <workavast@yandex.ru>
2025-04-24 10:01:28 +0300

    settings configs menuName bug fix

--- Assets/App/Scripts/Settings/Resolution/ResolutionSettingsConfig.cs
+++ Assets/App/Scripts/Settings/Resolution/ResolutionSettingsConfig.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace App.Settings.Resolution
 {
-    [CreateAssetMenu(fileName = nameof(ResolutionSettingsConfig), menuName = Consts.ConfigsPath + "/Settings/" + nameof(ResolutionSettingsConfig))]
+    [CreateAssetMenu(fileName = nameof(ResolutionSettingsConfig), menuName = Consts.ConfigsPath + "Settings/" + nameof(ResolutionSettingsConfig))]
     public class ResolutionSettingsConfig : ScriptableObject
     {
         [SerializeField] private List<InspectorResolution> resolutions;
