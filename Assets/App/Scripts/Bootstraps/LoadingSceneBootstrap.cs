1c903c59125ab0362607f1d295156c028d6d7c15
Rodion <workavast@yandex.ru>
2025-05-11 22:50:28 +0300

    bug fix on the client side

--- Assets/App/Scripts/Bootstraps/LoadingSceneBootstrap.cs
+++ Assets/App/Scripts/Bootstraps/LoadingSceneBootstrap.cs
@@ -1,3 +1,4 @@
+using App.NetworkRunning;
 using App.NetworkRunning.Shutdowners;
 using App.NetworkRunning.Shutdowners.LocalShutdowners;
 using Avastrad.ScenesLoading;
@@ -8,13 +9,23 @@ namespace App.Bootstraps
 {
     public class LoadingSceneBootstrap : MonoBehaviour
     {
+        [Inject] private readonly NetworkRunnerProvider _runnerProvider;
         [Inject] private readonly ShutdownerProvider _shutdownerProvider;
         [Inject] private readonly ISceneLoader _sceneLoader;
 
         private void Start()
         {
             _shutdownerProvider.SetLocalShutdownProvider(new DefaultShutdowner(_sceneLoader));
-            _sceneLoader.LoadTargetScene();
+
+            if (_runnerProvider.TryGetNetworkRunner(out var runner))
+            {
+                if (runner.IsServer) 
+                    _sceneLoader.LoadTargetScene();
+            }
+            else
+            {
+                _sceneLoader.LoadTargetScene();
+            }
         }
     }
 }
